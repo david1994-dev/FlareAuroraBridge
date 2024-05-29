d0a47a9e7458375f52ac92e1c6d610b67938e240
Diego Figueroa <diego.figueroa@aurora.dev>
2024-05-29 13:50:57 -0400

    chore: cleanup, add node version spec and build tweaks

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,35 +1,16 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
-const { owner_pk } = require('./config.json');
+import { owner_pk } from "./config.json";
 
 const config: HardhatUserConfig = {
-  defaultNetwork: "polygon",
-  // defaultNetwork: "aurora",
+  defaultNetwork: "amoy",
   networks: {
-    hardhat: {
-    },
-    // aurora: {
-    //   // url: "https://mainnet.aurora.dev/727b7d29cff3f12b1976b5a1bcf8e7c2b1bc612a8856b460ab9bbdfadd725ba",
-    //   // chainId: 1313161554,
-    //   // accounts: [owner_pk]
-
-
-    //   url: "https://testnet.aurora.dev",
-    //   chainId: 1313161555,
-    //   accounts: [owner_pk]
-    // },
-    polygon: {
-      // url: "https://polygon-rpc.com",
-      // chainId: 137,
-      // //gasPrice: 0,
-      // accounts: [owner_pk]
-
+    amoy: {
       url: "https://polygon-amoy-bor-rpc.publicnode.com",
       chainId: 80002,
-      //gasPrice: 0,
-      accounts: [owner_pk, owner_pk]
-    }
+      accounts: [owner_pk],
+    },
   },
   solidity: {
     version: "0.8.22",
